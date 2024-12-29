6b1d4c50aa5f6fad88544459fccadb4087ceead6
LarissaSL <larissa.silvaedge@gmail.com>
2024-12-29 16:40:43 -0300

    feat: loops, for, while, foreach, loop variable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,62 @@
 @extends('layouts.main_layout')
 @section('content')
-    {{-- Empty - avaliar se a variavel existe, se é 0/vazio/null --}}
-    @empty($values)
-        <p>Não Existe</p>
-    @else
-        <p>Existe</p>
-    @endempty
+    {{-- For --}}
+    @for ($i = 0; $i < 5; $i++)
+        <p>{{ $i }}
+        <p>
+    @endfor
 
-    {{-- Isset - Está ou não está definido a variável --}}
-    @isset($value)
-        <p>Existe a variável</p>
-    @else
-        <p>Não Existe a variável</p>
-    @endisset
+    {{-- Foreach - Percorrer uma coleção de dados --}}
+    @foreach ($cities as $city)
+        <p>{{ $city }}</p>
+    @endforeach
 
-    {{-- Unless - Verificar se uma condição não é verdadeira." --}}
-    @unless ($value == 100)
-        <p>OKKKK</p>
-    @endunless
+    {{-- Forelse --}}
+    @forelse ($names as $name)
+        <p>{{ $name }}</p>
+    @empty
+        <p>Names está vazio</p>
+    @endforelse
+
+    @while ($index < 6)
+        <p>Indice: {{ $index }}</p>
+        @php
+            $index++;
+        @endphp
+    @endwhile
+
+    {{-- Continue e Break--}}
+    {{-- For --}}
+    <h1>Continue e Break</h1>
+    @for ($i = 0; $i < 10; $i++)
+        {{-- Continue --}}
+        @if ($i == 2)
+            @continue
+        @endif
+        
+        <h1>{{ $i }}<h1>
+
+
+        {{-- Break --}}
+        @if ($i == 7)
+            @break
+        @endif
+    @endfor
+
+    {{-- Loop Variable --}}
+    @foreach ($cities as $city)
+        <p>City: {{ $city }}</p>
+        <p>Index: {{ $loop->index }}</p>
+        <p>Iteration: {{ $loop->iteration }}</p>
+        <p>Remaining: {{ $loop->remaining }}</p>
+        <p>Count: {{ $loop->count }}</p>
+        
+        @if($loop->first)
+            <p>Esta é a primeira iteração.</p>
+        @endif
+        
+        @if($loop->last)
+            <p>Esta é a última iteração.</p>
+        @endif
+    @endforeach
 @endsection
