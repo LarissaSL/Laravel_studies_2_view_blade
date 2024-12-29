42fc432c7d4b2396f72f94e7cf9ccba9a8628e7c
LarissaSL <larissa.silvaedge@gmail.com>
2024-12-29 14:21:50 -0300

    feat: condicional simples, encadeada nas views

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,29 +9,6 @@ class MainController extends Controller
 {
     public function showView(): View
     {
-        // // Método 1 (Array Associativo)
-        // $data = [
-        //     'name' => 'Larissa Silva',
-        //     'phone' => '1233445334'
-        // ];
-
-        // return view('admin.newPage3',$data);
-
-        // Método 2 (Array Associativo diretamente no parametro)
-        // return view('admin.newPage3',[
-        //     'name' => 'Larissa Silva',
-        //     'phone' => '1233445334'
-        // ]);
-
-        // Método 3 (função with)
-        // return view('admin.newPage3')
-        //         ->with('name', 'Larissa Silva')
-        //         ->with('phone', '123456789');
-
-        // Método 4 (Compact)
-        $name = 'Larissa Sousa';
-        $phone = '12345567889';
-
-        return view('admin.newPage3', compact('name', 'phone'));
+        return view('home', ['value' => 100]);
     }
 }
